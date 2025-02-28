b0930477c4c2d37c80ad64b37582f498d83b021c
Gabrieldamasceno <juragabriel21@gmail.com>
2025-02-28 12:41:18 -0300

    Criação do projeto caixa eletrônico

---
+++ src/Main/UserData.java
@@ -0,0 +1,14 @@
+package Main;
+
+import java.util.Scanner;
+
+public abstract class UserData {
+    private String userName;
+    private String userAge;
+    private String userGender;
+
+    public void readUserData(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Digite");
+    }
+}
